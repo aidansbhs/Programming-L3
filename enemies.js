33a54691f3c6a2c9842cb50d590fd684149599ee
Aidan McAra <51344785+aidansbhs@users.noreply.github.com>
2021-10-19 15:12:31 +1300

    Fixed mage projectile
    Explained in project ticket

--- enemies.js
+++ enemies.js
@@ -119,9 +119,8 @@ class Tank {
     }
 }
 
-
 class Mage {
-    constructor(x, y, w, h, c, xSpeed, ySpeed, maxProjectiles = 1) {
+    constructor(x, y, w, h, c, xSpeed, ySpeed, maxProjectiles = 1, maxInitialPs = 1) {
         this.x = x;
         this.y = y;
         this.w = w;
@@ -129,6 +128,8 @@ class Mage {
         this.c = c;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
+        this.initialPs = [];
+        this.maxInitialPs = maxInitialPs;
         this.maxProjectiles = maxProjectiles;
         this.projectiles = [];
         this.setup = true;
@@ -138,10 +139,10 @@ class Mage {
         canvasContext.fillRect(this.x, this.y, this.w, this.h);
     }
 
-    distance(){
+    distance() {
         let y = this.y - player.y;
         let x = this.x - player.x;
-                
+
         return Math.sqrt(x * x + y * y); //calculates distance between mage and player
     }
 
@@ -152,24 +153,54 @@ class Mage {
             this.setup = false;
         }
 
-        this.x -= this.xSpeed;
-
-        // mages.forEach(function(item){
-                if(this.distance() <= 800){ 
-                console.log('in range');
-                this.xSpeed = 0;
-            } else {
-                console.log('not in range');
-                this.x -= this.xSpeed;
+        if (this.distance() <= 600) {
+            inRange = true;
+            this.xSpeed = 0;
+        } else {
+            if (this.x > player.x + player.w) {
+                this.x -= this.xSpeed; //both directions
             }
-        // });
+            if (this.x + this.w < player.x) {
+                this.x += this.xSpeed;
+            }
+        }
+        if (this.distance() > 600) {
+            inRange = false;
+            if (this.x > player.x + player.w) {
+                this.xSpeed = 4;
+            }
+            if (this.x + this.w < player.x) {
+                this.xSpeed = -4;
+            }
+        }
+
+        if (difficulty == 'hard') {
+            if (this.distance() <= 150 && teleported == false) {
+                //disapear
+                teleported = true;
+                //wait 0.5 seconds
+                this.x = Math.floor(Math.random() * ((canvas.width - this.w) - (0) + (canvas.width - this.w)));
+                //reapear
+            }
+        }
+
+
     }
     shooting() {
         if (gameState = 'playing') {
-            if (this.x < canvas.width - this.w) {
-                if ((this.distance() <= 800) && this.xSpeed == 0) {
+            if (this.x < canvas.width - this.w) { //inside canvas
+                if (inRange == true) {
+                    if (this.initialPs.length < this.maxInitialPs) {
+                        this.initialPs.push(new mageInitial(player.x, player.y, 50, "white"));
+                        //wait for one second
+                    }
                     if (this.projectiles.length < this.maxProjectiles) { //limits spamming
-                        this.projectiles.push(new mageProjectile(player.x, player.y));
+
+                        setTimeout(() => {
+                            // this.projectiles.push(new mageProjectile(temp8[0].x, temp8[0].y, 50, "red"));
+                            this.projectiles.push(new mageProjectile(this.initialPs[this.initialPs.length -1].x, this.initialPs[this.initialPs.length-1].y, 50, "red"));
+                        }, 1000); //shoots to the initial circles coordinates after delay
+
                     }
                 }
             }
